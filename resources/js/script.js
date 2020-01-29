3b2ca90a07b11b3da3de18ebec394e1564d1c403
Peter <pitrens.studio@gmail.com>
2020-01-30 00:10:56 +0800

    animation to nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Sticky navigation
   $(".js--section-features").waypoint(
     function(direction) {
       if (direction === "down") {
@@ -9,4 +10,49 @@ $(document).ready(function() {
     },
     { offset: "95%" }
   );
+
+  // Scroll on buttons
+  $(".js--scroll-to-plans").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".js--section-plans").offset().top
+      },
+      1000
+    );
+  });
+
+  $(".js--scroll-to-start").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".js--section-features").offset().top
+      },
+      1000
+    );
+  });
+
+  // Navigation scrolling
+
+  $(function() {
+    $("a[href*=\\#]:not([href=\\#])").click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            2000
+          );
+          return false;
+        }
+      }
+    });
+  });
 });
